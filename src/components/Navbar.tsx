41296b1ed8feba66dd2f17e66206e24623bc0edd
Rananda Dian Ilham <nanda@eluon.com>
2023-01-10 16:02:03 +0700

    ueueu

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { Container, Nav, Navbar as NavbarBs, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+
+export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart();
+
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ position: "relative", width: "3rem", height: "3rem" }}
+            variant="outline-primary"
+            className="rounded-circle"
+          >
+            {" "}
+            <img
+              src="/cart.svg"
+              style={{
+                width: "2.5rem",
+                height: "2.5rem",
+                top: 0,
+                left: 0,
+                transform: "translate(-20%, -10%)",
+              }}
+            />
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.0rem",
+                height: "1.0rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, -10%)",
+                fontSize: '0.5em'
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavbarBs>
+  );
+}
---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,62 @@
+import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
+
+  return (
+    <Card className="h-100 ">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+        className="noselect"
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="fs-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ add to cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-item-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button className="noselect" onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div className="noselect">
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button className="noselect" onClick={() => increaseCartQuantity(id)}>+</Button>
+              </div>
+              <Button className="noselect" variant="danger" size="sm" onClick={() => removeFromCart(id)}>
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
